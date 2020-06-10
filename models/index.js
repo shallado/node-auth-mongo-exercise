022420a30f396ba07e91b428f0d1f70288807e11
Sandro Hallado <sandrohallado@yahoo.com>
2020-06-09 19:21:41 -0700

    Create roles data on successful connection to database

--- models/index.js
+++ models/index.js
@@ -3,16 +3,37 @@ const roleModel = require('./role')(mongoose);
 const userModel = require('./user')(mongoose);
 const { url } = require('../config/db');
 
+const roles = ['user', 'moderator', 'admin'];
+
+const initial = () => {
+  const Role = roleModel;
+
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      roles.forEach((role) => {
+        const newRole = new Role({ name: role });
+
+        newRole.save()
+          .then((roleData) => console.log(roleData))
+          .catch((err) => console.log({ message: err }));
+      });
+    }
+  });
+};
+
 mongoose.connect(url, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 })
-  .then(() => console.log('Successfully connected to database'))
-  .catch((err) => console.log('Unable to connect to database'));
+  .then(() => {
+    console.log('Successfully connected to database');
+    initial();
+  })
+  .catch((err) => console.log('Unable to connect to database', err));
 
 module.exports = {
   mongoose,
   roleModel,
   userModel,
-  roles: ['user', 'moderator', 'admin']
+  roles
 };
